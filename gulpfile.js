6fb358050839b041460a2c2cdc3e5a6f40d163b8
qazsato <qaz.s.co@gmail.com>
2016-02-08 00:39:58 +0900

    Change gulp settings

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,6 @@ var gulp        = require('gulp');
 var $           = require('gulp-load-plugins')();
 var del         = require('del');
 var runSequence = require('run-sequence');
-var browserify  = require('browserify');
-var through2    = require('through2');
 var fs          = require('fs');
 var path        = require('path');
 var browserSync = require('browser-sync');
@@ -25,8 +23,7 @@ var SRC = {
   JS     : ['public/src/**/*.js'],
   CSS    : ['public/src/**/*.css'],
   SASS   : ['public/src/**/*.scss'],
-  IMAGE  : ['public/src/**/*.+(jpg|jpeg|png|gif|svg)', '!public/src/**/sprite/*.png'],
-  SPRITE : ['public/src/**/sprite/*.png'],
+  IMAGE  : ['public/src/**/*.+(jpg|jpeg|png|gif|svg)'],
   OTHER  : ['public/src/**/*.*', '!public/src/**/*.+(js|css|scss|jpg|jpeg|png|gif|svg)']
 };
 
@@ -36,21 +33,12 @@ var DEST = {
 
 /**
  * JSタスク。
- * srcフォルダの.jsに対し、require/uglifyをおこないdistフォルダへ出力します。
+ * srcフォルダの.jsに対し、uglifyをおこないdistフォルダへ出力します。
  * このタスクは開発・本番ともに使用します。(build/watchタスクでの使用を想定)
  */
 gulp.task('js', function () {
-  var browserified = through2.obj(function(file, encode, callback){
-    browserify(file.path).bundle(function(err, res){
-      file.contents = res;
-      callback(null, file);
-    });
-  });
   gulp.src(SRC.JS)
       .pipe($.plumber({errorHandler: $.notify.onError('Error: <%= error.message %>')}))
-      // .pipe($.sourcemaps.init())
-      .pipe(browserified)
-      // .pipe($.sourcemaps.write())
       .pipe($.if(ENV === 'production', $.uglify()))
       .pipe(gulp.dest(DEST.ROOT));
 });
@@ -77,11 +65,9 @@ gulp.task('css', function () {
 gulp.task('sass', function () {
   gulp.src(SRC.SASS)
       .pipe($.plumber({errorHandler: $.notify.onError('Error: <%= error.message %>')}))
-      // .pipe($.sourcemaps.init())
       .pipe($.sass())
       .pipe($.if(ENV === 'production', $.cssmin()))
       .pipe($.autoprefixer({browsers: AUTOPREFIXER_BROWSERS}))
-      // .pipe($.sourcemaps.write())
       .pipe(gulp.dest(DEST.ROOT));
 });
 
@@ -96,45 +82,6 @@ gulp.task('image', function () {
       .pipe($.plumber({errorHandler: $.notify.onError('Error: <%= error.message %>')}))
       .pipe($.imagemin())
       .pipe(gulp.dest(DEST.ROOT));
-});
-
-/**
- * Spriteタスク
- * spriteフォルダ直下にある画像を、スプライト画像に変換しdistフォルダへ出力します。
- * このタスクは開発・本番ともに使用します。(build/watchタスクでの使用を想定)
- */
-gulp.task('sprite', function () {
-  // spriteフォルダを再帰的に探索する
-  var spriteEach = function(p, callback) {
-    fs.readdir(p, function(err, files) {
-      if (err) return;
-      files.forEach(function(f) {
-        var fp = path.join(p, f);
-        if(fs.statSync(fp).isDirectory()) {
-          if (f === 'sprite') {
-            callback(fp);
-          } else {
-            spriteEach(fp, callback);
-          }
-        }
-      });
-    });
-  };
-  spriteEach(SRC.ROOT, function(path) {
-    var srcPath = path + '/*.png';                                // 対象となる画像群
-    var imgPath = path.replace('public/src', '') + '/sprite.png'; // 出力する画像名称
-    var destImgPath = path.replace('/src/', '/dist/');            // スプライト画像の出力先
-    var destCssPath = path.replace('/images/', '/styles/');       // スプライトCSSの出力先
-    var spriteData = gulp.src(srcPath)
-                         .pipe($.spritesmith({
-                           imgName:   'sprite.png',
-                           imgPath:   imgPath,
-                           cssName:   '_sprite.scss',
-                           cssFormat: 'scss'
-                         }));
-    spriteData.img.pipe(gulp.dest(destImgPath));
-    spriteData.css.pipe(gulp.dest(destCssPath));
-  });
 });
 
 /**
@@ -189,7 +136,6 @@ gulp.task('watch', function () {
   gulp.watch(SRC.CSS, ['css']);
   gulp.watch(SRC.SASS, ['sass']);
   gulp.watch(SRC.IMAGE, ['image']);
-  gulp.watch(SRC.SPRITE, ['sprite']);
   gulp.watch(SRC.OTHER, ['other']);
 });
 
@@ -199,5 +145,5 @@ gulp.task('watch', function () {
  * このタスクは開発・本番ともに使用します。
  */
 gulp.task('build', function () {
-  runSequence('clean', 'sprite', ['js', 'css', 'sass', 'image', 'other']);
+  runSequence('clean', ['js', 'css', 'sass', 'image', 'other']);
 });
