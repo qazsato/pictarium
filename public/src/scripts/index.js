b1b4d63e63ee228de78b2bbb0d1eba20b0ff20aa
qazsato <qaz.s.co@gmail.com>
2016-02-14 17:57:11 +0900

    Mod top ui

--- public/src/scripts/index.js
+++ public/src/scripts/index.js
@@ -11,6 +11,8 @@ $(function () {
   });
 
   $('#upload-btn').on('click', function () {
+    var $btn = $(this);
+    $btn.attr("disabled", true);
     var data = $('#picture').attr('src');
     $.ajax({
       type: "POST",
@@ -20,9 +22,11 @@ $(function () {
         // TODO 送信成功時の処理
         $('#thanks-area').show();
         $('#intro-area,#picture-area').hide();
+        $btn.removeAttr("disabled");
       },
       error: function (e) {
         // TODO 送信失敗時の処理
+        $btn.removeAttr("disabled");
       }
     });
 
