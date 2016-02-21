07b6e1e9faf842573f191f13bdfeae59e12c2cd1
qazsato <qaz.s.co@gmail.com>
2016-02-22 02:30:01 +0900

    Add exif

--- public/src/scripts/index.js
+++ public/src/scripts/index.js
@@ -1,15 +1,20 @@
 $(function () {
   // 写真選択時
   $('#image-file').on('change', function () {
+    var file = this.files[0];
     var reader = new FileReader();
-    reader.readAsDataURL(this.files[0]);
+    reader.readAsDataURL(file);
     reader.onload = function () {
-      var image = new Image();
-      image.src = reader.result;
-      image.onload = function() {
-        $('#picture').attr('src', resizeImage(this, 0.5));
-        showPictureView();
-      };
+      // MEMO iOS端末だと画像向きが変わってしまうため、EXIF情報から適切な向きに変換する。
+      EXIF.getData(file, function(){
+        var orientation = file.exifdata.Orientation;
+        var image = new Image();
+        image.src = reader.result;
+        image.onload = function() {
+          $('#picture').attr('src', resizeImage(this, orientation, 0.5));
+          showPictureView();
+        };
+      });
     };
   });
 
@@ -40,20 +45,30 @@ $(function () {
 
 /**
  * 写真をリサイズします。
- * スマホの写真は数MBを超えるので、アップロード時の負荷軽減のためリサイズ実施。
+ * スマホの写真は数MBを軽く超えるので、アップロード時の負荷軽減のためリサイズ実施。
  * @param  {Image} image Imageオブジェクト
+ * @param  {Number} orientation EXIFの画像向き情報(1-8)
  * @param  {Number} scale リサイズ値(0-1)
  * @return {String}       Base64
  */
-var resizeImage = function (image, scale) {
+var resizeImage = function (image, orientation, scale) {
   var canvas = document.createElement('canvas');
-  canvas.width = image.width * scale;
-  canvas.height = image.height * scale;
+  var width = image.width * scale;
+  var height = image.height * scale;
+  if (orientation === 6) {
+    canvas.width = height;
+    canvas.height = width;
+  } else {
+    canvas.width = width;
+    canvas.height = height;
+  }
   var context = canvas.getContext('2d');
-  // MEMO iOS端末だと画像向きが変わってしまうケースがあるため、EXIF情報から適切な向きに変換する。
-  // context.rotate(90 * Math.PI / 180);
-  // context.translate(0, -1 * canvas.height);
-  context.drawImage(image, 0, 0, canvas.width, canvas.height);
+  if (orientation === 6) {
+    // 時計回りに90度回転
+    context.rotate(90 * Math.PI / 180);
+    context.translate(0, -1 * height);
+  }
+  context.drawImage(image, 0, 0, width, height);
   return canvas.toDataURL();
 };
 
