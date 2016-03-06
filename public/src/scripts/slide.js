fb845775505bf8a7acb67208e8aaefc3bc7627a3
qazsato <qaz.s.co@gmail.com>
2016-03-07 01:53:59 +0900

    Add animation / Add interval

--- public/src/scripts/slide.js
+++ public/src/scripts/slide.js
@@ -1,13 +1,29 @@
-var pictArray = [];
 $(function () {
-  $.ajax({
-    type: 'GET',
-    url: '/picture/get',
-    success: function(data){
-      pictArray = data;
-      $('img').attr('src', pictArray[pictArray.length - 1]);
-    },
-    error: function (e) {
+  var pictArray = [];
+  var pictNum = 0;
+
+  // 画像データ取得
+  setInterval(function () {
+    $.ajax({
+      type: 'GET',
+      url: '/picture/get',
+      success: function(data){
+        pictArray = data;
+      },
+      error: function (e) {
+      }
+    });
+  }, 10000);
+
+  // 画像データ表示
+  setInterval(function () {
+    $('img')
+      .attr('src', pictArray[pictNum])
+      .animation('fadeIn');
+    if (pictNum < pictArray.length) {
+      pictNum++;
+    } else {
+      pictNum = 0;
     }
-  });
+  }, 5000);
 });
