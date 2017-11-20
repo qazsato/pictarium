b4a14e9aff1d48cc9b0548dd6d0aa13d31cbb66b
qazsato <qazsato@gmail.com>
2017-11-20 09:02:03 +0900

    Element更新に伴う修正

--- public/src/user/main.js
+++ public/src/user/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue';
 import App from './App.vue'
 import Element from 'element-ui';
 import locale from 'element-ui/lib/locale/lang/ja';
-import 'element-ui/lib/theme-default/index.css';
+import 'element-ui/lib/theme-chalk/index.css';
 Vue.use(Element, {locale});
 
 window.onload = function () {
