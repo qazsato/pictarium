e4fcc843c41fdc2b0a295e10b0d875324db52aa2
qazsato <qazsato@gmail.com>
2017-11-26 23:23:16 +0900

    送信ボタン押下でS3に画像を保存

--- public/src/user/App.vue
+++ public/src/user/App.vue
@@ -2,10 +2,25 @@
   <div id="app">
     <img v-show="uploadedImage" :src="uploadedImage"/>
     <input type="file" @change="onFileChange">
+    <button @click="submitPhoto">送信</button>
   </div>
 </template>
 
 <script>
+import AWS from 'aws-sdk';
+import EXIF from 'exif-js';
+const ALBUM_BUCKET_NAME = 'pictarium-photos';
+AWS.config.update({
+  region: 'ap-northeast-1',
+  credentials: new AWS.CognitoIdentityCredentials({
+    IdentityPoolId: 'ap-northeast-1:e961eef1-1852-4238-968f-322ce60b3c90'
+  })
+});
+const s3 = new AWS.S3({
+  apiVersion: '2006-03-01',
+  params: {Bucket: ALBUM_BUCKET_NAME}
+});
+
 export default {
   name: 'app',
   data() {
@@ -15,8 +30,8 @@ export default {
   },
   methods: {
     onFileChange(e) {
-      const files = e.target.files || e.dataTransfer.files;
-      this.createImage(files[0]);
+      this.file = e.target.files[0] || e.dataTransfer.files[0];
+      this.createImage(this.file);
     },
     createImage(file) {
       const reader = new FileReader();
@@ -25,7 +40,19 @@ export default {
       };
       reader.readAsDataURL(file);
     },
-  },
+    submitPhoto() {
+      s3.upload({
+        Key: this.file.name,
+        Body: this.file,
+        ACL: 'public-read'
+      }, (err, data) => {
+        if (err) {
+          return alert('There was an error uploading your photo: ', err.message);
+        }
+        alert('Successfully uploaded photo.');
+      });
+    }
+  }
 }
 </script>
 
