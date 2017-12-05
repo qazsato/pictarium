4a735b21ee32f6679a5976fec197d27d77b1181f
qazsato <qazsato@gmail.com>
2017-12-05 21:02:24 +0900

    カメラボタンコンポーネント化

--- public/src/components/AppTop.vue
+++ public/src/components/AppTop.vue
@@ -9,45 +9,40 @@
           <el-button class="cancel-button" @click="cancelPhoto">キャンセル</el-button>
         </div>
       </section>
-      <el-button id="camera-button" :class="{'slide-in-up':isCameraButtonShow === true, 'slide-in-down':isCameraButtonShow === false}">
-        <div class="camera-text">撮影する</div>
-        <i class="camera-icon material-icons">photo_camera</i>
-        <input type="file" @change="onFileChange">
-      </el-button>
+      <camera-button :onSelectImage="selectImage" :is-show="isCameraButtonShow"></camera-button>
     </main>
   </div>
 </template>
 
 <script>
 import PicHeader from './Header.vue';
+import CameraButton from './CameraButton.vue';
 import Photo from '../utils/Photo';
 const photo = new Photo();
 
 export default {
   name: 'app',
   components: {
-    PicHeader
+    PicHeader,
+    CameraButton
   },
   data() {
     return {
       loading: false,
+      file: null,
       uploadedImage: null,
       isCameraButtonShow: true
     };
   },
   methods: {
-    onFileChange(e) {
-      this.file = e.target.files[0] || e.dataTransfer.files[0];
-      this.createImage(this.file);
-      this.isCameraButtonShow = false;
-      e.target.value = '';  // NOTE 同一ファイルの選択イベント発火を検知できるよう初期状態にする。
-    },
-    createImage(file) {
+    selectImage(file) {
+      this.file = file;
       const reader = new FileReader();
+      reader.readAsDataURL(file);
       reader.onload = (e) => {
         this.uploadedImage = e.target.result;
+        this.isCameraButtonShow = false;
       };
-      reader.readAsDataURL(file);
     },
     submitPhoto() {
       this.loading = true;
@@ -96,44 +91,6 @@ export default {
   .el-message {
     top: 60px;
   }
-
-  .slide-in-up,
-  .slide-in-down {
-    animation-duration: .2s;
-    animation-fill-mode: both;
-  }
-
-  @keyframes slide-in-up {
-    from {
-      transform: translate3d(0, 100%, 0);
-      visibility: visible;
-    }
-
-    to {
-      transform: translate3d(0, 0, 0);
-      visibility: visible;
-    }
-  }
-
-  .slide-in-up {
-    animation-name: "slide-in-up";
-  }
-
-  @keyframes slide-in-down {
-    from {
-      transform: translate3d(0, 0, 0);
-      visibility: visible;
-    }
-
-    to {
-      transform: translate3d(0, 100%, 0);
-      visibility: hidden;
-    }
-  }
-
-  .slide-in-down {
-    animation-name: "slide-in-down";
-  }
 </style>
 
 <style lang="postcss" scoped>
@@ -167,42 +124,5 @@ export default {
     color: #c0bebe;
     border: 1px solid #c0bebe;
     background-color: #fff;
-  }
-
-  #camera-button {
-    position: fixed;
-    bottom: 8px;
-    right: 8px;
-    height: 80px;
-    width: 80px;
-    padding: 0;
-    border: none;
-    border-radius: 50%;
-    color: #fff;
-    background-color: #ff5555;
-    box-shadow: 0px 0px 26px -8px rgba(0, 0, 0, 0.7);
-  }
-
-  #camera-button:hover {
-    background-color: #ff2222;
-  }
-
-  .camera-text {
-    margin: 12px 0 2px 0;
-    font-size: 13px;
-  }
-
-  .camera-icon {
-    font-size: 32px;
-  }
-
-  #camera-button input[type=file] {
-    position: absolute;
-    top: 0;
-    left: 0;
-    z-index: 100;
-    height: 100%;
-    width:  100%;
-    opacity: 0;
   }
 </style>
---
+++ public/src/components/CameraButton.vue
@@ -0,0 +1,100 @@
+<template>
+  <el-button id="camera-button" :class="{'slide-in-up':isShow === true, 'slide-in-down':isShow === false}">
+    <div class="camera-text">撮影する</div>
+    <i class="camera-icon material-icons">photo_camera</i>
+    <input type="file" @change="onFileChange">
+  </el-button>
+</template>
+
+<script>
+export default {
+  props: ['onSelectImage', 'isShow'],
+  methods: {
+    onFileChange(e) {
+      const file = e.target.files[0] || e.dataTransfer.files[0];
+      this.onSelectImage(file);
+      e.target.value = '';  // NOTE 同一ファイルの選択イベント発火を検知できるよう初期状態にする。
+    }
+  }
+}
+</script>
+
+<style lang="postcss">
+  .slide-in-up,
+  .slide-in-down {
+    animation-duration: .2s;
+    animation-fill-mode: both;
+  }
+
+  @keyframes slide-in-up {
+    from {
+      transform: translate3d(0, 100%, 0);
+      visibility: visible;
+    }
+
+    to {
+      transform: translate3d(0, 0, 0);
+      visibility: visible;
+    }
+  }
+
+  .slide-in-up {
+    animation-name: "slide-in-up";
+  }
+
+  @keyframes slide-in-down {
+    from {
+      transform: translate3d(0, 0, 0);
+      visibility: visible;
+    }
+
+    to {
+      transform: translate3d(0, 100%, 0);
+      visibility: hidden;
+    }
+  }
+
+  .slide-in-down {
+    animation-name: "slide-in-down";
+  }
+</style>
+
+
+<style lang="postcss" scoped>
+  #camera-button {
+    position: fixed;
+    bottom: 8px;
+    right: 8px;
+    height: 80px;
+    width: 80px;
+    padding: 0;
+    border: none;
+    border-radius: 50%;
+    color: #fff;
+    background-color: #ff5555;
+    box-shadow: 0px 0px 26px -8px rgba(0, 0, 0, 0.7);
+  }
+
+  #camera-button:hover {
+    background-color: #ff2222;
+  }
+
+  .camera-text {
+    margin: 12px 0 2px 0;
+    font-size: 13px;
+  }
+
+  .camera-icon {
+    font-size: 32px;
+  }
+
+  #camera-button input[type=file] {
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: 100;
+    height: 100%;
+    width:  100%;
+    opacity: 0;
+  }
+</style>
