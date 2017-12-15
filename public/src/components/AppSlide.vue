960846a9d02cd885cce969d8551513b0cd564f70
qazsato <qazsato@gmail.com>
2017-12-16 01:52:10 +0900

    不要なコメント削除

--- public/src/components/AppSlide.vue
+++ public/src/components/AppSlide.vue
@@ -64,7 +64,7 @@ export default {
     transition: opacity .5s
   }
 
-  .fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  .fade-enter, .fade-leave-to {
     opacity: 0
   }
 </style>
