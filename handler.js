fb748a79930eed5f94be10827a3b790b9ad39b09
qazsato <qazsato@gmail.com>
2018-04-05 08:58:41 +0900

    ランタイムをNode.js v8.10に更新

--- handler.js
+++ handler.js
@@ -2,7 +2,6 @@
 
 const AWS = require('aws-sdk');
 const s3 = new AWS.S3();
-const co = require('co');
 const gm = require('gm').subClass({imageMagick: true});
 
 module.exports.resize = (event, context, callback) => {
@@ -10,14 +9,14 @@ module.exports.resize = (event, context, callback) => {
   const Key = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, ' '));
   const fileName = Key.split('/')[Key.split('/').length - 1];
   const imageType = Key.match(/\.([^.]*)$/)[0];
-  co(function *() {
-    const photo = yield s3.getObject({Bucket, Key}).promise();
-    const smallBuffer = yield resizePhoto(240, 240, photo, imageType);
-    const largeBuffer = yield resizePhoto(1000, 1000, photo, imageType);
-    yield s3.putObject({Bucket, Key: `small/${fileName}`, Body: smallBuffer, ContentType: photo.ContentType}).promise();
-    yield s3.putObject({Bucket, Key: `large/${fileName}`, Body: largeBuffer, ContentType: photo.ContentType}).promise();
+  (async () => {
+    const photo = await s3.getObject({Bucket, Key}).promise();
+    const smallBuffer = await resizePhoto(240, 240, photo, imageType);
+    const largeBuffer = await resizePhoto(1000, 1000, photo, imageType);
+    await s3.putObject({Bucket, Key: `small/${fileName}`, Body: smallBuffer, ContentType: photo.ContentType}).promise();
+    await s3.putObject({Bucket, Key: `large/${fileName}`, Body: largeBuffer, ContentType: photo.ContentType}).promise();
     callback(null, 'success');
-  })
+  })()
   .catch((e) => callback(e));
 }
 
